c90ea1a338cd0bf2b7deda5a36a78908adffc2e2
bweppl1@wgu.edu <bweppl1@wgu.edu>
2025-08-18 10:11:28 -0600

    sds

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/toddler-learning-games",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
