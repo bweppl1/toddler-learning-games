c17545353f90dc73b459386a0490916ebfff3745
bweppl1@wgu.edu <bweppl1@wgu.edu>
2025-08-25 21:18:28 -0600

    auth progress

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,25 @@
 import User from "../models/User.js";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //login user
 export const loginUser = async (req, res) => {
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //sign up
@@ -13,7 +29,9 @@ export const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -19,9 +20,45 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// static login method
+
+userSchema.statics.login = async function (email, password) {
+  //validating email and password
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("This email is not registered!");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password!");
+  }
+
+  return user;
+};
+
 // static signup method
 userSchema.statics.signup = async function (email, password) {
-  // checking if email exists
+  //validating email and password
+  if (!email || !password) {
+    throw Error("All fields must be filled!");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("This is not a valid email!");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough!");
+  }
+
+  // checking if email exists in the database
   const exists = await this.findOne({ email });
 
   if (exists) {
